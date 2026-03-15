2c527bbb2f12b23ea8c5953e5c2d945d2f7f4e84
niyeee4 <niyee4@gmail.com>
2026-03-16 01:30:08 +0700

    Update trapcode.sh

--- trapcode.sh
+++ trapcode.sh
@@ -1,5 +1,5 @@
 cd
 cd ~/.wine
-wget -O - 'https://github.com/niyeee4/Cs6Termux/releases/download/TrapcodeSuite(Update)/trapcode.suite.tar.xz' | pv | tar -xJf -
+wget -O - https://github.com/niyeee4/Cs6Termux/releases/download/TrapcodeSuite\(Update\)/trapcode.suite.tar.xz | pv | tar -xJf -
 cd
 curl -sL https://raw.githubusercontent.com/niyeee4/Cs6Termux/refs/heads/main/trapcode2.sh | bash
