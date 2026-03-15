4cbac40217a387668e2d50eb7d77f2036ee505e8
niyeee4 <niyee4@gmail.com>
2026-03-15 19:46:56 +0700

    Create trapcodesuite.sh

---
+++ trapcodesuite.sh
@@ -0,0 +1,5 @@
+cd ~
+cd ~/.wine
+wget -O - https://github.com/niyeee4/Cs6Termux/releases/download/TrapcodeSuite/trapcodesuite.tar.xz | pv | tar -xJf -
+rm -rf "drive_c/Program Files/Adobe/Common/Plug-ins/CS6/MediaCore/RGSGrowBounds.aex"
+cd ~
