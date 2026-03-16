72cc6898343440b2ceeb102d69da94a75abacb50
niyeee4 <niyee4@gmail.com>
2026-03-16 09:17:15 +0700

    Update trapcode2.sh

--- trapcode2.sh
+++ trapcode2.sh
@@ -27,7 +27,8 @@ sleep 2
 DISPLAY=:1 WINE_DISABLE_KERNEL_WRITEWATCH=1 wineboot
 sleep 2
 
-DISPLAY=:1 WINE_DISABLE_KERNEL_WRITEWATCH=1 wine explorer /desktop=install,1920x1080 msiexec /i C:\\TrapcodeSuite13.exe
+DISPLAY=:1 WINE_DISABLE_KERNEL_WRITEWATCH=1 wine explorer /desktop=install,1920x1080 C:\\TrapcodeSuite13.exe
+
 wineserver -w
 wineserver -k
 pkill -f com.termux.x11
