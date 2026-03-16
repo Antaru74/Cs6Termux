ed4a9dd700ee5acdebabfa13e9d2cb1264a9b73a
niyeee4 <niyee4@gmail.com>
2026-03-16 13:12:02 +0700

    Delete trapcodesuite.sh

--- trapcodesuite.sh
+++
@@ -1,5 +0,0 @@
-cd ~
-cd ~/.wine
-wget -O - https://github.com/niyeee4/Cs6Termux/releases/download/TrapcodeSuite/trapcodesuite.tar.xz | pv | tar -xJf -
-rm -rf "drive_c/Program Files/Adobe/Common/Plug-ins/CS6/MediaCore/RGSGrowBounds.aex"
-cd ~
