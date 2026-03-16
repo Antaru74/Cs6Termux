4b8e45f31181e195d34b59ce5a0624aa6ab961a2
niyeee4 <niyee4@gmail.com>
2026-03-16 08:39:57 +0700

    Update trapcode2.sh

--- trapcode2.sh
+++ trapcode2.sh
@@ -28,29 +28,6 @@ DISPLAY=:1 WINE_DISABLE_KERNEL_WRITEWATCH=1 wineboot
 sleep 2
 
 DISPLAY=:1 WINE_DISABLE_KERNEL_WRITEWATCH=1 wine explorer /desktop=install,1920x1080 msiexec /i C:\\TrapcodeSuite13.exe
-
 wineserver -w
-
-echo
-echo "type y to continue"
-
-while true; do
-read confirm < /dev/tty
-[ "$confirm" = "y" ] && break
-echo "type y to continue"
-done
-
 wineserver -k
 pkill -f com.termux.x11
-
-cd ~/.wine/drive_c
-
-rm -rf TrapcodeSuite13.exe
-
-cp -r "$HOME/.wine/drive_c/Program Files/Adobe/Adobe After Effects CS6/Support Files" \
-"$HOME/.wine/drive_c/Adobe After Effects CS6 Portable/App/Ae/"
-
-rm -rf "$HOME/.wine/drive_c/Program Files/Adobe/Adobe After Effects CS6/Support Files"/*
-rm -rf "$HOME/.wine/drive_c/Program Files/Adobe/Common/Plug-ins/CS6/MediaCore/RGSGrowBounds.aex"
-
-echo DONE
